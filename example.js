7eff04021aa816e5774e9e8e922275b70d2898dd
不四 <busi.hyy@taobao.com>
2014-02-25 23:09:41 +0800

    init middlewares

---
+++ example.js
@@ -0,0 +1,27 @@
+/*!
+ * koa-middlewares - example.js
+ * Copyright(c) 2014 dead_horse <dead_horse@qq.com>
+ * MIT Licensed
+ */
+
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+
+var koa = require('koa');
+var middlewares = require('./');
+
+var app = koa();
+
+app.use(middlewares.bodyParser());
+app.use(middlewares.jsonp());
+app.use(middlewares.router(app));
+middlewares.csrf(app);
+
+app.use(function *() {
+  this.body = 'hello koa-middlewares';
+});
+
+app.listen(7001);
---
+++ index.js
@@ -0,0 +1,25 @@
+/*!
+ * koa-middlewares - index.js
+ * Copyright(c) 2014 dead_horse <dead_horse@qq.com>
+ * MIT Licensed
+ */
+
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+
+exports.bodyParser = require('koa-bodyparser');
+exports.render = require('koa-ejs');
+exports.session = require('koa-sess');
+exports.RedisStore = require('koa-redis');
+exports.staticCache = require('koa-static-cache');
+exports.rt = require('koa-rt');
+exports.jsonp = require('koa-jsonp');
+exports.router = require('koa-router');
+exports.ResourceRouter = require('koa-resource-router');
+exports.csrf = require('koa-csrf');
+exports.logger = require('koa-logger');
+exports.favicon = require('koa-favicon');
+exports.rewrite = require('koa-rewrite');
